34530f0dd4016e1cd36c0f9849c7fcbe3cfa26e4
Martynas-J <Aurimhas@gmail.com>
2023-06-24 23:06:34 +0300

    add gallery v1.0

--- src/Pages/GalleryPage/GalleryPage.js
+++ src/Pages/GalleryPage/GalleryPage.js
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { API_URL } from "../../Components/Config/Config";
+import { toast } from "react-toastify";
 
 const GalleryPage = () => {
+  const [system, setSystem] = useState('');
+  const [discoverers, setDiscoverers] = useState('');
+  const [planets, setPlanets] = useState('');
+
+  useEffect(() => {
+    axios.get(`${API_URL}/planets?_embed=photos`)
+      .then(res => setPlanets(res.data))
+      .catch(res => toast.error(res.message))
+      axios.get(`${API_URL}/discoverers?_embed=photos`)
+      .then(res => setDiscoverers(res.data))
+      .catch(res => toast.error(res.message))
+
+  }, [])
+
+if (!discoverers || !planets) {
+  return ""
+}
+
+console.log(discoverers)
+console.log(planets)
   return (
     <div className="gallery-wrapper">
       <h1 className="page-title">Gallery</h1>
-      </div>
+    </div>
   )
 }
 
