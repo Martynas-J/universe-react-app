fa4b51527c1fb6a11b19cc0bbfa9c6ae37bda7f7
Martynas-J <Aurimhas@gmail.com>
2023-06-24 18:19:13 +0300

    add one planet

--- src/Pages/PlanetPage/PlanetPage.js
+++ src/Pages/PlanetPage/PlanetPage.js
@@ -2,26 +2,30 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { API_URL } from "../../Components/Config/Config";
 import { toast } from "react-toastify";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const PlanetPage = () => {
     const [planet, setPlanet] = useState('');
     const { id } = useParams();
 
     useEffect(() => {
-        axios.get(`${API_URL}/planets/${id}?_embed=photos`)
+        axios.get(`${API_URL}/planets/${id}?_expand=discoverer&_expand=system&_embed=photos`)
             .then(res => setPlanet(res.data))
             .catch(res => toast.error(res.message))
     }, [])
     if (!planet) {
         return ""
     }
-    const { name, country, birthplace, occupation, contribution, photos } = planet
+    console.log(planet)
+    const { name, photos, discoverer, discovererId, system, systemId, galaxy, } = planet
 
     return (
         <div className="planet">
             {photos[0] ? <img className="medium-img" src={photos[0].url}></img> : ""}
-            <h2 className="planet-title" > {name}</h2>
+            <h2 className="planet-title"> {name}</h2>
+            <p>
+                The scientist who made the most significant contributions to the discovery is <Link to={`/discoverers/${discovererId}`}>{discoverer.name}</Link>. Earth belongs to the <Link to={`/systems/${systemId}`}>{system.name}</Link> system, which is located in the {galaxy} Galaxy.
+            </p>
 
         </div>
     )
