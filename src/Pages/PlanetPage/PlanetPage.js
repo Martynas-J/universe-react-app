42fb8382bfd97c6076b1c72ff7b40c404c0c98c6
Martynas-J <Aurimhas@gmail.com>
2023-06-24 20:12:29 +0300

    add one planet v1.1

--- src/Pages/PlanetPage/PlanetPage.js
+++ src/Pages/PlanetPage/PlanetPage.js
@@ -17,14 +17,21 @@ const PlanetPage = () => {
         return ""
     }
     console.log(planet)
-    const { name, photos, discoverer, discovererId, system, systemId, galaxy, } = planet
+    const { name, photos, discoverer, discovererId, system, systemId, galaxy, satellites} = planet
+    const starText = system.stars.length > 1 ?"stars" : "star"
+    let satellitesText = ""
+    if (satellites.length > 1) {
+        satellitesText = `Natural satellites are ` + satellites.join(", ")
+    }else if (satellites.length === 1) {
+        satellitesText = `Natural satellite is ` + satellites[0]
+    }
 
     return (
         <div className="planet">
             {photos[0] ? <img className="medium-img" src={photos[0].url}></img> : ""}
             <h2 className="planet-title"> {name}</h2>
             <p>
-                The scientist who made the most significant contributions to the discovery is <Link to={`/discoverers/${discovererId}`}>{discoverer.name}</Link>. Earth belongs to the <Link to={`/systems/${systemId}`}>{system.name}</Link> system, which is located in the {galaxy} Galaxy.
+                The scientist who made the most significant contributions to the discovery is <Link to={`/discoverers/${discovererId}`}>{discoverer.name} {discoverer.occupation}</Link>. Earth belongs to the <Link to={`/systems/${systemId}`}>{system.name}</Link> ({system.stars.length} {starText}) system, which is located in the {galaxy} Galaxy. {name} {satellitesText}.
             </p>
 
         </div>
