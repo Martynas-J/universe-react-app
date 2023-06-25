043689575fc266ab3e37088a12e591921881a2c8
Martynas-J <Aurimhas@gmail.com>
2023-06-25 06:44:10 +0300

    add gallery v1.2

--- src/Pages/GalleryPage/GalleryPage.js
+++ src/Pages/GalleryPage/GalleryPage.js
@@ -12,15 +12,46 @@ const GalleryPage = () => {
       .catch(res => toast.error(res.message))
   }, [])
 
-if (!photos) {
-  return ""
-}
+  if (!photos) {
+    return ""
+  }
+  const stars = photos.filter(photo => photo.category === "stars").slice(0, 5)
+  const discoverers = photos.filter(photo => photo.category === "discoverers").slice(0, 5)
+  const planets = photos.filter(photo => photo.category === "planets").slice(0, 5)
+  const systems = photos.filter(photo => photo.category === "systems").slice(0, 5)
 
-console.log(photos)
+console.log(stars)
+
+  const starsPhotosElement = stars.length > 0 ?
+    <div className="stars-photos">
+      <h2>Stars</h2>
+      {stars.map(star => <img className="medium-img" key={star.id} src={star.thumbnailUrl}></img>)}
+    </div> : ""
+  const discoverersPhotosElement = discoverers.length > 0 ?
+    <div className="discoverers-photos">
+      <h2>Discoverers</h2>
+      {discoverers.map(discoverer => <img className="medium-img" key={discoverer.id} src={discoverer.thumbnailUrl}></img>)}
+    </div> : ""
+  const planetsPhotosElement = planets.length > 0 ?
+    <div className="planets-photos">
+      <h2>Planets</h2>
+      {planets.map(planet => <img className="medium-img" key={planet.id} src={planet.thumbnailUrl}></img>)}
+    </div> : ""
+  const systemsPhotosElement = systems.length > 0 ?
+    <div className="systems-photos">
+      <h2>Systems</h2>
+      {systems.map(system => <img className="medium-img" key={system.id} src={system.thumbnailUrl}></img>)}
+    </div> : ""
 
   return (
     <div className="gallery-wrapper">
       <h1 className="page-title">Gallery</h1>
+      {starsPhotosElement}
+      {discoverersPhotosElement}
+      {planetsPhotosElement}
+      {systemsPhotosElement}
+
+
     </div>
   )
 }
