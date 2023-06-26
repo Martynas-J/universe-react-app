b51135e8104e65c626c1efd2c0af57693a96dc0c
Martynas-J <Aurimhas@gmail.com>
2023-06-26 21:45:10 +0300

    add home Page

--- src/Components/MainNavBar/MainNavBar.js
+++ src/Components/MainNavBar/MainNavBar.js
@@ -3,43 +3,41 @@ import "./MainNavBar.scss"
 import { useState } from 'react';
 import MenuItem from '../MenuItem/MenuItem';
 
-const MainNavBar = () => {
+const MainNavBar = ({ isHomePage }) => {
     const [menuOn, setMenuOn] = useState(false)
     const addFormHandler = () => {
         setMenuOn(prevState => !prevState)
     }
     return (
-        <>
-            <nav className='main-navigation'>
-                <ul className='nav-list'>
-                    <li className='navigation-item'>
-                        <NavLink to='/' className='navigation-link'>Home</NavLink>
-                    </li>
-                    <li className='navigation-item'>
-                        <NavLink to='/systems' className='navigation-link'>Systems</NavLink>
-                    </li>
-                    <li className='navigation-item'>
-                        <NavLink to='/stars' className='navigation-link'>Stars</NavLink>
-                    </li>
-                    <li className='navigation-item'>
-                        <NavLink to='/planets' className='navigation-link'>Planets</NavLink>
-                    </li>
-                    <li className='navigation-item'>
-                        <NavLink to='/discoverers' className='navigation-link'>Discoverers</NavLink>
-                    </li>
-                    <li className='navigation-item'>
-                        <NavLink to='/gallery' className='navigation-link'>Gallery</NavLink>
-                    </li>
-                    {menuOn ? <li><MenuItem /></li> : ""}
-                    <li>
-                        <button className='logo-button' onClick={addFormHandler}>
-                            <img className='logo' src='https://icon-library.com/images/hamburger-menu-icon-svg/hamburger-menu-icon-svg-16.jpg'></img>
-                        </button>
-                    </li>
+        <nav className={isHomePage ? 'main-navigation home-page' : 'main-navigation'}>
+            <ul className='nav-list'>
+                <li className='navigation-item'>
+                    <NavLink to='/' className='navigation-link'>Home</NavLink>
+                </li>
+                <li className='navigation-item'>
+                    <NavLink to='/systems' className='navigation-link'>Systems</NavLink>
+                </li>
+                <li className='navigation-item'>
+                    <NavLink to='/stars' className='navigation-link'>Stars</NavLink>
+                </li>
+                <li className='navigation-item'>
+                    <NavLink to='/planets' className='navigation-link'>Planets</NavLink>
+                </li>
+                <li className='navigation-item'>
+                    <NavLink to='/discoverers' className='navigation-link'>Discoverers</NavLink>
+                </li>
+                <li className='navigation-item'>
+                    <NavLink to='/gallery' className='navigation-link'>Gallery</NavLink>
+                </li>
+                {menuOn ? <li><MenuItem /></li> : ""}
+                <li>
+                    <button className='logo-button' onClick={addFormHandler}>
+                        <img className='logo' src='https://icon-library.com/images/hamburger-menu-icon-svg/hamburger-menu-icon-svg-16.jpg'></img>
+                    </button>
+                </li>
 
-                </ul>
-            </nav>
-        </>
+            </ul>
+        </nav>
     )
 }
 
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,13 +1,53 @@
 import Container from "../../Components/Container/Container"
-
+import "./HomePage.scss"
 
 const HomePage = () => {
   return (
     <Container>
       <div className="home-wrapper">
         <h1 className="page-title">Home</h1>
-      </div>
-    </Container>
+        <div className="home-content">
+          <p >
+            The universe is a vast expanse that contains numerous celestial objects, and one way to categorize these objects is based on their classification as planets. Planets are large bodies that orbit around stars, such as our own Sun. They are characterized by their gravitational force and their spherical shape. The universe is home to a multitude of planets, with over 100 known planets discovered so far.
+          </p>
+          <p>
+            Planets can vary significantly in size, temperature, and composition. They are composed of gases, liquids, and solids. These celestial bodies may have atmospheres, surfaces, rocks, and even water. Some planets have their own moons and revolve around their host star. They can exhibit diverse characteristics, such as having a solid surface like Earth or being predominantly gaseous like Jupiter.
+          </p>
+          <p>
+            The categorization of planets in the universe is an ongoing scientific endeavor. Scientists have classified planets into different categories based on their characteristics. One common categorization includes terrestrial planets, gas giants, and icy or rocky planets. Terrestrial planets, like Earth, are primarily composed of solid materials and have a relatively thin atmosphere. Gas giants, such as Jupiter and Saturn, are predominantly made up of gases and lack a well-defined solid surface. Icy or rocky planets, like Pluto, have a significant amount of ice or rock in their composition.
+          </p>
+          <p>
+            Understanding the distribution and classification of planets in the universe is crucial for unraveling the mysteries of our cosmic environment. It provides insights into the diversity and complexity of celestial bodies and their formation processes. Astronomers and researchers continue to explore and discover new planets, expanding our knowledge of the universe and the planetary systems beyond our own.
+          </p>
+          <p>
+            As our exploration of the universe advances, the quest to understand the distribution and characteristics of planets will undoubtedly continue to captivate the minds of scientists and space enthusiasts alike. The study of planets not only expands our knowledge of the universe but also sparks intriguing questions about the possibility of life beyond our own planet.
+          </p>
+          <ul>
+            <li>Asteroids</li>
+            <li>Comets</li>
+            <li>Moons</li>
+            <li>Dwarf Planets</li>
+            <li>Satellites</li>
+            <li>Planets</li>
+            <li>Star Systems</li>
+            <li>Star Clusters</li>
+            <li>Galaxy Groups</li>
+            <li>Galaxies</li>
+            <li>Galaxy Clusters</li>
+          </ul>
+          <p>The given list encompasses various celestial bodies and structures. Here is a brief description of each category:</p>
+          <p>1. Asteroids: Small rocky objects that orbit the Sun, mostly found in the asteroid belt between Mars and Jupiter.</p>
+          <p>2. Comets: Icy bodies that orbit the Sun and exhibit a coma (a glowing atmosphere) and a tail as they approach the Sun.</p>
+          <p>3. Moons: Natural satellites that orbit planets and other celestial bodies, providing companionship to their hosts.</p>
+          <p>4. Dwarf Planets: Celestial bodies that orbit the Sun and resemble planets but have not cleared their orbit from other debris.</p>
+          <p>5. Satellites: Artificial objects or natural bodies that orbit planets, moons, or other celestial bodies.</p>
+          <p>6. Planets: Large celestial bodies that orbit a star, typically spherical in shape and have cleared their orbit from other debris.</p>
+          <p>7. Star Systems: A group of stars bound together by gravitational force, often consisting of multiple stars and other celestial objects.</p>
+          <p>8. Star Clusters: An assemblage of stars that is a gravitational union and has a common origin, varying in size and shape.</p>
+          <p>9. Galaxy Groups: A collection of galaxies that are gravitationally bound and share a common origin, varying in size and shape.</p>
+        </div>
+      </div >
+    </Container >
 
   )
 }
